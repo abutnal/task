4d27da16959aa9d387f560aecb7dbea2f37e3bbb
abutnal <45108373+abutnal@users.noreply.github.com>
2019-08-20 18:20:30 +0530

    Update task.php

--- task.php
+++ task.php
@@ -13,12 +13,12 @@ function passByValue($num, $divisibleByA, $divisibleByB, $ReturnValueIfTrue){
 function excuse($start, $end){
   $returnString = "";
   for ($i = $start;$i <= $end;$i++) {
-    $ValueToAdd = $i;
-    $ValueToAdd= passByValue($ValueToAdd, 5, 3, "Linianos");
-    $ValueToAdd= passByValue($ValueToAdd, 5, 1, "IT");
-    $ValueToAdd= passByValue($ValueToAdd, 3, 1, "Linio");
+    $addVal = $i;
+    $addVal= passByValue($addVal, 5, 3, "Linianos");
+    $addVal= passByValue($addVal, 5, 1, "IT");
+    $addVal= passByValue($addVal, 3, 1, "Linio");
    
-    $returnString.= '<table border="1" style="margin-left:300px; padding:"><tr><td>'.$ValueToAdd . "</tr></td></table><br>";
+    $returnString.= '<table border="1" style="margin-left:300px; padding:"><tr><td>'.$addVal . "</tr></td></table><br>";
   }
   return $returnString;
 }
